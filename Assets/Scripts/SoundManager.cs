b974be3b102f5a8fe352fc7131cf03a286c7d8d0
SmallPeenwow <bowdencamryn@gmail.com>
2023-02-21 13:34:28 +0200

    Polish - Number Popup Animation Done

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -81,6 +81,11 @@ public class SoundManager : MonoBehaviour
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
     }
+    
+    public void PlayCountdownSound()
+    {
+        PlaySound(audioClipRefsSO.warning, Vector3.zero);
+    }
 
     public void ChangeVolume()
     {
